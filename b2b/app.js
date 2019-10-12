2ab07f84015e35e22b75fde81b26cf5c25a8bbc8
Lauren Mellon <laumellon@gmail.com>
2019-10-12 14:41:17 -0400

    added form and functions to update header based on input in form

--- b2b/app.js
+++ b2b/app.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
         return(
             <div>
-                <h1>TO DOs</h1>
+                <h1># of TO DOs on my list: {this.props.todoCount}</h1>
             </div>
         )
     }
@@ -21,28 +21,56 @@ class TaskList extends React.Component {
 
 
 class IndexView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-        description: '',
-        complete: false
+    constructor(props) {
+        super(props);
+        this.state={
+            todoList: [],
+            todoCount: 0,
+            description: '',
+            complete: false
+        }
     }
-  }
-  render() {
-    return (
-        <div>
-            <Header />
-            <TaskList />
-            <form>
-                <input
-                type='text'
-                value={this.state.description}
-                placeholder='Add another task' />
-                <input type='submit' value='Add' />
-            </form>
-        </div>
-    );
-  }
+
+    handleChange = (event) => {
+        this.setState( {description: event.target.value} )
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const newTask = {
+            description: this.state.description,
+            complete: this.state.complete
+        }
+        //console.log(newTask);
+        const updatedTodoList = [newTask, ...this.state.todoList]
+        //console.log(updatedTodoList);
+        const updatedCount = updatedTodoList.length
+        //console.log(updatedCount);
+        this.setState({
+            todoList: updatedTodoList,
+            todoCount: updatedCount,
+            description: ''
+        })
+        console.log(this.state.todoList);
+        console.log(this.state.todoCount);
+    }
+
+    render() {
+        return (
+            <div>
+                <Header todoCount={this.state.todoCount} />
+                <TaskList todoList={this.state.todoList} />
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                    type='text'
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                    placeholder='Add another task' />
+                    <input type='submit' value='Add' />
+                </form>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
